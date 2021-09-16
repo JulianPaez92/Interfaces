5e89b3736e9cafdb9f49315f022b82070215a52e
JulianPaez92 <70548446+JulianPaez92@users.noreply.github.com>
2021-09-16 01:28:08 -0300

    Create prueba.js

---
+++ clase4/prueba.js
@@ -0,0 +1,9 @@
+alert("Ejercicio 1")
+var nombre = prompt("Cuál es tu nombre?")
+var edad = prompt("Cuál es tu edad?")
+var cumple = prompt("Cuándo es tu cumple?")
+var ciudad = prompt("En qué ciudad vivís?")
+var ocupacion = prompt("Cuál es tu ocupación?")
+
+console.log("Mi nombre es " + nombre + ", tengo " + edad + " años y cumplo el "  + cumple + "" )
+console.log("Vivo en " + ciudad + " y trabajo como " + ocupacion)
